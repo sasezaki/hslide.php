c2f2acc4c477653f2228e2e972b5427d831fb618
anatoo <anatoo.jp@gmail.com>
2011-10-18 00:06:08 +0900

    Removed __DIR__ constant.

--- hslide.php
+++ hslide.php
@@ -1,11 +1,11 @@
 <?php
-require_once __DIR__ . '/lib/HatenaSyntax.php';
-require_once __DIR__ . '/lib/Hslide.php';
+require_once dirname(__FILE__) . '/lib/HatenaSyntax.php';
+require_once dirname(__FILE__) . '/lib/Hslide.php';
 
 $slide_text = '';
-foreach (glob(__DIR__ . '/slides/*.txt') as $path) {
+foreach (glob(dirname(__FILE__) . '/slides/*.txt') as $path) {
     $slide_text .= file_get_contents($path);
 }
 
-$hs = new Hslide(__DIR__ . '/template.php');
+$hs = new Hslide(dirname(__FILE__) . '/template.php');
 echo $hs->render($slide_text);
